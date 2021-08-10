f8255359cd3dc0dcaab72dccb39039907ba9c2b7
Raghav <raghuweer23@gmail.com>
2021-08-10 18:10:58 +0100

    Added functionalitites such as extracting current MAC Address using regex and print statements if the code falls into some error

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 from optparse import OptionParser
+import re
 
 
 def get_arguments():
@@ -25,6 +26,29 @@ def change_mac(interface, new_mac):
     subprocess.run(["ifconfig", interface, "up"])
 
 
+def get_curr_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+
+    mac_address_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+
+    if mac_address_result:
+        return mac_address_result.group(0)
+
+    else:
+        print("[-] could not read MAC Address ")
+
+
 options = get_arguments()
+
+curr_mac = get_curr_mac(options.interface)
+print("current MAC address = " + str(curr_mac))
+
 change_mac(options.interface, options.new_mac)
+
+curr_mac = get_curr_mac(options.interface)
+
+if curr_mac == options.new_mac:
+    print("[-] MAC address changed successfully")
+else:
+    print("[-] ERROR MAC address did not change")
 
